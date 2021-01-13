b1a26c94166057c8d4f7a21ee6c43fca38f28bb1
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2021-01-13 16:25:22 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function($) {
+  
+ 
+  //tabs
+//   $('[data-can]').on('click', function() {
+//       $(this).addClass('active').siblings().removeClass('active').closest('.can__wrap').find('ul.can__list').removeClass('active').eq($(this).index()).addClass('active');
+//   });
+ 
+  //slider
+//   if (jQuery('[data-writers]').length > 0) {
+//       $('[data-writers]').slick({
+//           dots: true,
+//           speed: 300,
+//           slidesToShow: 2,
+//           responsive: [{
+//               breakpoint: 768,
+//               settings: {
+//                   slidesToShow: 1
+//               }
+//           }, ]
+//       });
+//   }
+  
+  // faq
+//   if (jQuery('[data-faq]').length > 0) {
+//       $('[data-faq]').click(function() {
+//           $(this).find('.faq__title').toggleClass('open');
+//           $(this).find('p').slideToggle(300);
+//       })
+//   }
+  // burger
+//   $('[data-burger]').click(function() {
+//       $('html').toggleClass("open");
+//       $(this).toggleClass("open");
+//       $('[data-nav]').toggleClass("open");
+//       $('body').toggleClass('open');
+//   });
+  
+});
